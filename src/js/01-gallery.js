6320de83a97d15de7ac3e5421d7c820de514869b
Nikita0708 <91466168+Nikita0708@users.noreply.github.com>
2022-11-21 14:48:59 +0200

    qwe

---
+++ src/js/01-gallery.js
@@ -0,0 +1,29 @@
+// Add imports above this line
+import { galleryItems } from "../js/gallery-items";
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsMarkup = createGalerryItemsM(galleryItems);
+
+galleryEl.innerHTML = galleryItemsMarkup;
+
+function createGalerryItemsM(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}" />
+        </a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
